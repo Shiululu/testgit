d2b5bb7c4690aed26c76b34b0f522fed1cb0cf89
weber <weber122462118@gmail.com>
2022-09-16 19:21:53 +0800

    dffffffffffffffffffff

--- src/main/java/tw/YEE/Yee.java
+++ src/main/java/tw/YEE/Yee.java
@@ -3,6 +3,6 @@ package tw.YEE;
 public class Yee {
 
 	public void yee() {
-		System.out.println("YEEEEEEEEEEEEEpaaaEEEEEE22222222222EEEEEEEEEEEEEEEEEE");
+		System.out.println("YEEEEEEEEEEEEEpaaaEEEEEE22222222fffffffffffffffffff222EEEEEEEEEEEEEEEEEE");
 	}
 }
